34e56da008d3c0c3ba72e8a4f83448c7eb2565a5
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2022-05-31 01:08:57 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react'
+import './styles.css'
+
+import {Card} from "../../components/Card";
+
+export function Home() {
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name: '', avatar: ''})
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+    setStudents(prevState =>[...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/ValdineiJunior').then(response => response.json().then(data => {
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url
+      })
+
+    }))
+  }, [students])
+  
+  return ( 
+    <div className='container'>
+      <header>
+      <h1>Lista de Presença</h1>
+<div><strong>{user.name}</strong>
+<img src={user.avatar} alt="Foto de perfil" /></div>
+      </header>
+    <input
+     type="text"
+     placeholder="Digite o nome..."
+     onChange={e => setStudentName( e.target.value)}
+     />
+    <button type="button" onClick={handleAddStudent}>Adcionar</button>
+
+    {/* a key usada aqui não é ideal, como ela precisa ser unica, e estamos usando o time, caso adcionamos dois nomes no mesmo segundo, a chave sera duplicada, aparecendo o erro no console. */}
+
+    {
+      students.map(student => <Card key={student.time} name={student.name} time= {student.time} />)
+    }
+    </div>
+   )
+}
